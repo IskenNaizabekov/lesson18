abdb6a4cc9caa12a3f6d1dca8452842aea6a8bdd
IskenNaizabekov <iskennaizabekov@gmail.com>
2025-05-15 14:47:09 +0600

    изм

--- js/lesson.js
+++ js/lesson.js
@@ -39,3 +39,49 @@ tabsParents.onclick = (event) => {
         });
     }
 };
+
+const somInput = document.querySelector('#som');
+const usdInput = document.querySelector('#usd');
+const eurInput = document.querySelector('#eur');
+
+const converter = (element1, element2, element3) => {
+    element1.oninput = () => {
+        const request = new XMLHttpRequest();
+        request.open('GET', '../data/converter.json');
+        request.setRequestHeader('Content-type', 'application/json');
+        request.send();
+
+        request.onload = () => {
+            const data = JSON.parse(request.response);
+            const value = parseFloat(element1.value);
+
+            if (element1.value === '') {
+                element2.value = '';
+                element3.value = '';
+                return;
+            }
+
+            if (element1.id === 'som') {
+                element2.value = (value / data.usd).toFixed(2);
+                element3.value = (value / data.eur).toFixed(2);
+            }
+            if (element1.id === 'usd') {
+                const somValue = value * data.usd;
+                element2.value = somValue.toFixed(2);
+                element3.value = (somValue / data.eur).toFixed(2);
+            }
+            if (element1.id === 'eur') {
+                const somValue = value * data.eur;
+                element2.value = somValue.toFixed(2);
+                element3.value = (somValue / data.usd).toFixed(2);
+            }
+        }
+    }
+}
+
+converter(somInput, usdInput, eurInput);
+converter(usdInput, somInput, eurInput);
+converter(eurInput, somInput, usdInput);
+
+
+
